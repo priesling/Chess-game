1b7499eae513b37f75548be310efe706b617f9aa
lqmanh <luongquangmanh85@gmail.com>
2017-05-12 18:33:56 +0700

    Proposed basic structure of the project

---
+++ sample/chesspieces.py
@@ -0,0 +1,33 @@
+class ChessPiece:
+    def __init__(self):
+        pass
+
+
+class Pawn(ChessPiece):
+    def __init__(self):
+        super().__init__()
+
+
+class Rook(ChessPiece):
+    def __init__(self):
+        super().__init__()
+
+
+class Bishop(ChessPiece):
+    def __init__(self):
+        super().__init__()
+
+
+class Knight(ChessPiece):
+    def __init__(self):
+        super().__init__()
+
+
+class Queen(ChessPiece):
+    def __init__(self):
+        super().__init__()
+
+
+class King(ChessPiece):
+    def __init__(self):
+        super().__init__()
